e60cbd08eed9fec3bd32bbc1e05b7d561531b28e
atjapan2015@yahoo.co.jp <atjapan2015@yahoo.co.jp>
2019-05-02 08:23:48 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,50 @@
+
+# Helidon Freemarker MP Example
+
+This example implements a simple Hello World Web Application using Helidon + MicroProfile + Mustache
+
+## Prerequisites
+
+1. Maven 3.5 or newer
+2. Java SE 8 or newer
+
+Verify prerequisites
+```
+java -version
+mvn --version
+```
+
+## Build
+
+```
+mvn package
+```
+
+## Start the application
+
+```
+java -jar target/helidon-mp-mvc-mustache.jar
+```
+
+## Exercise the application
+
+http://localhost:8080/web/index
+
+## Try health and metrics
+
+```
+curl -s -X GET http://localhost:8080/health
+{"outcome":"UP",...
+. . .
+
+# Prometheus Format
+curl -s -X GET http://localhost:8080/metrics
+# TYPE base:gc_g1_young_generation_count gauge
+. . .
+
+# JSON Format
+curl -H 'Accept: application/json' -X GET http://localhost:8080/metrics
+{"base":...
+. . .
+
+```
---
+++ src/main/java/io/helidon/webserver/mp/web/controller/WebResource.java
@@ -0,0 +1,62 @@
+package io.helidon.webserver.mp.web.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.glassfish.jersey.server.mvc.Template;
+import org.glassfish.jersey.server.mvc.Viewable;
+import org.slf4j.Logger;
+
+import io.helidon.webserver.mp.entity.HelidonBean;
+
+@Path("/web")
+@RequestScoped
+public class WebResource {
+
+	@Inject
+	transient Logger logger;
+
+	@Path("/index")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public Viewable goIndex() {
+
+		logger.info("######goIndex()######");
+		Map<String, String> model = new HashMap<>();
+		model.put("hello", "Hello");
+		model.put("world", "World");
+
+		return new Viewable("/index", model);
+	}
+
+	@GET
+	// @ErrorTemplate(name = "/error.mustache")
+	@Template(name = "/helidon.mustache")
+	@Path("/helidon")
+	@Produces(MediaType.TEXT_HTML)
+	public Map<String, Object> getAllHelidonSites() {
+
+		logger.info("######getAllHelidonSites()######");
+		List<HelidonBean> helidonSites = new ArrayList<>();
+
+		helidonSites.add(new HelidonBean("Helidon Project", "https://helidon.io"));
+		helidonSites.add(new HelidonBean("Helidon Docs", "https://helidon.io/docs"));
+		helidonSites.add(new HelidonBean("Helidon API Docs",
+				"https://helidon.io/docs/latest/apidocs/index.html?overview-summary.html"));
+		helidonSites.add(new HelidonBean("Helidon Github", "https://github.com/oracle/helidon"));
+
+		Map<String, Object> model = new HashMap<>();
+		model.put("helidonSites", helidonSites);
+		return model;
+	}
+
+}
